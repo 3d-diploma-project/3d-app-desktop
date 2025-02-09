2e5b0460a3be82c61ced46d61d23144f6e1365a2
VadimST04 <vadikstarchak04@gmail.com>
2025-02-09 17:42:28 +0100

    find model center

--- src/main/java/org/cmps/tetrahedron/model/Model.java
+++ src/main/java/org/cmps/tetrahedron/model/Model.java
@@ -2,6 +2,7 @@ package org.cmps.tetrahedron.model;
 
 import lombok.*;
 import org.cmps.tetrahedron.utils.DataReader;
+import org.joml.Vector3f;
 
 import java.io.File;
 import java.util.*;
@@ -14,4 +15,23 @@ public class Model {
 
     private Map<Integer, float[]> vertices;
     private List<float[][]> faces;
+
+    public Vector3f getModelCenter() {
+        if (vertices == null || vertices.isEmpty()) {
+            throw new RuntimeException("Model has no vertices");
+        }
+
+        float sumX = 0, sumY = 0, sumZ = 0;
+        int vertexCount = vertices.size();
+
+        for (float[] vertex : vertices.values()) {
+            sumX += vertex[0];
+            sumY += vertex[1];
+            sumZ += vertex[2];
+        }
+
+        System.err.println("Model Center: " + sumX / vertexCount + ", " + sumY / vertexCount + ", " + sumZ / vertexCount);
+        return new Vector3f(sumX / vertexCount, sumY / vertexCount, sumZ / vertexCount);
+    }
+
 }
